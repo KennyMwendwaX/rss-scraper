d7ca3cad1cf6c47e83f2ea2f26e3e7ba77908762
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-22 19:52:17 +0300

    feat: scope the readiness api url to only GET requests

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	}))
 
 	v1Router := chi.NewRouter()
-	v1Router.HandleFunc("/readiness", handlerReadiness)
+	v1Router.Get("/readiness", handlerReadiness)
 
 	router.Mount("/v1", v1Router)
 
