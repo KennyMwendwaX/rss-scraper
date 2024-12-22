1304202c487e1471b5af42b5d85d0414246dc145
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-22 19:59:41 +0300

    feat: add handler error helper function

--- json.go
+++ json.go
@@ -17,3 +17,15 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	w.Write(data)
 }
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	if code > 499 {
+		log.Println("Responding with 5XX level error:", message)
+	}
+
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errorResponse{Error: message})
+}
