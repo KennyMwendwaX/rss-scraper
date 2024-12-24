e1f9eb298c3cc9bd947e2417e028b45d8b178c7f
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-24 20:33:36 +0300

    feat: implement GetUser handler to get a user from the db

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/KennyMwendwaX/rss-scrapper/internal/auth"
 	"github.com/KennyMwendwaX/rss-scrapper/internal/database"
 	"github.com/KennyMwendwaX/rss-scrapper/internal/models"
 	"github.com/google/uuid"
@@ -50,45 +52,19 @@ func (apiConfig *ApiConfig) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, models.SerializeUser(user))
+	respondWithJSON(w, http.StatusCreated, models.SerializeUser(user))
 }
 
 func (apiConfig *ApiConfig) GetUser(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Name string `json:"name"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-
-	params := parameters{}
-	err := decoder.Decode(&params)
+	apiKey, err := auth.GetAPIKey(r.Header)
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Error parsing json")
+		respondWithError(w, http.StatusUnauthorized, fmt.Sprintf("Authorization error: %v", err))
 		return
 	}
 
-	// Convert uuid.UUID to pgtype.UUID
-	id := uuid.New()
-	pgID := pgtype.UUID{
-		Bytes: id, // Assign the UUID bytes
-		Valid: true,
-	}
-
-	// Convert time.Time to pgtype.Timestamp
-	now := time.Now().UTC()
-	pgTimestamp := pgtype.Timestamp{
-		Time:  now,  // Assign the time value
-		Valid: true, // Set to valid
-	}
-
-	user, err := apiConfig.DB.CreateUser(r.Context(), database.CreateUserParams{
-		ID:        pgID,
-		Name:      params.Name,
-		CreatedAt: pgTimestamp,
-		UpdatedAt: pgTimestamp,
-	})
+	user, err := apiConfig.DB.GetUserByAPIKey(r.Context(), apiKey)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Error creating user")
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error getting user: %v", err))
 		return
 	}
 
