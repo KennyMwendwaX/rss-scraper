f9d3989e9ef3d038247f70d7282750713a74ee67
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-24 20:50:28 +0300

    feat: add feeds tableschema sql

---
+++ internal/sql/schema/003_feeds.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+
+Create TABLE feeds (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE  
+);
