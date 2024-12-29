26c9e5f2a05f27fc4864d615b843b758ca75c3d0
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-30 00:17:59 +0300

    feat: add feed_follow functionality with SQL queries and model definitions using sqlc generate

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -12,7 +12,8 @@ import (
 )
 
 const createFeed = `-- name: CreateFeed :one
-INSERT INTO feeds (id, name, url, created_at, updated_at, user_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, name, url, created_at, updated_at, user_id
+INSERT INTO feeds (id, name, url, created_at, updated_at, user_id) 
+VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, name, url, created_at, updated_at, user_id
 `
 
 type CreateFeedParams struct {
