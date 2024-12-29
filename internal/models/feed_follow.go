c5b8d2a00cc596ad0208c51f8eece98c24db7f7b
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-12-30 00:28:05 +0300

    feat: add FeedFollow model to represent user feed subscriptions

---
+++ internal/models/feed_follow.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/KennyMwendwaX/rss-scrapper/internal/database"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type FeedFollow struct {
+	ID        pgtype.UUID      `json:"id"`
+	CreatedAt pgtype.Timestamp `json:"created_at"`
+	UpdatedAt pgtype.Timestamp `json:"updated_at"`
+	UserID    pgtype.UUID      `json:"user_id"`
+	FeedID    pgtype.UUID      `json:"feed_id"`
+}
+
+func FromDatabaseFeedFollow(feedFollow database.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID:        feedFollow.ID,
+		CreatedAt: feedFollow.CreatedAt,
+		UpdatedAt: feedFollow.UpdatedAt,
+		UserID:    feedFollow.UserID,
+		FeedID:    feedFollow.FeedID,
+	}
+}
